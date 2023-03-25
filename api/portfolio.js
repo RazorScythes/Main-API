ae66c4dc6be32ce549722234f9d1ba07d638289b
RazorScythes <unknownidentity257@gmail.com>
2023-03-26 00:59:38 +0800

    cors

--- api/portfolio.js
+++ api/portfolio.js
@@ -1,6 +1,6 @@
 const express           = require('express')
 const router            = express.Router()
-const { allowCors }     = require('../cors')
+// const { allowCors }     = require('../cors')
 
 const { uploadHero, getPortfolio } = require('../controller/portfolio')
 
