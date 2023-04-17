a753aba95f4b122dced0403443a2986b2dfe4c0e
RazorScythes <unknownidentity257@gmail.com>
2023-04-17 22:59:47 +0800

    added settings

--- api/settings.js
+++ api/settings.js
@@ -3,8 +3,8 @@ const router              = express.Router()
 
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', req.header('origin'))
-    //res.setHeader('Access-Control-Allow-Origin', '*')
+    // res.setHeader('Access-Control-Allow-Origin', req.header('origin'))
+    res.setHeader('Access-Control-Allow-Origin', '*')
     // another common pattern
     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
