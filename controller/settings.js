0e86a0cb629b84569b7d658d6ef66a2c78051aca
RazorScythes <unknownidentity257@gmail.com>
2023-06-09 23:52:08 +0800

    game page

--- controller/settings.js
+++ controller/settings.js
@@ -188,7 +188,7 @@ function deleteSingleImage (delete_id, folder) {
 }
 
 exports.userToken = async (req, res) => {
-    const token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.token
     
     jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
         if (err) {
